99f493b045630ad62783e462382245572c7badb5
Jason <jasonhuggins11@outlook.com>
2022-07-30 20:09:26 +0100

    Rename constant

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,11 @@
-const container = document.querySelector(".grid-container");
+const gridContainer = document.querySelector(".grid-container");
 
 // Create 16x16 rows
 function createGrid() {
     for (let i = 0; i < 256; i++) {
         const cell = document.createElement("div");
         cell.className = "cell";
-        container.appendChild(cell);
+        gridContainer.appendChild(cell);
     }
 }
 
