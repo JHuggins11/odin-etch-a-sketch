5983db4dd63997e1228c408d82ec06b26417af65
Jason <jasonhuggins11@outlook.com>
2022-07-30 19:11:04 +0100

    Add function to create 16x16 grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,1 +1,17 @@
 const container = document.querySelector("#container");
+
+// Create 16x16 grid
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+
+        for (let j = 0; j < 16; j++) {
+            const cell = document.createElement("div");
+            cell.className = "cell";
+            row.appendChild(cell);
+        }
+
+        container.appendChild(row);
+    }
+}
